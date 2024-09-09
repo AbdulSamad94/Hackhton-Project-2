8c28844536af4586f3498d397fe3c8f1d6f5f8fb
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-09-09 12:48:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var form = document.getElementById('detailsForm');
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    // Collect form values
+
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
     var email = document.getElementById('email').value;
@@ -11,7 +11,7 @@ form.addEventListener('submit', function (event) {
     var experience1 = document.getElementById('experience1').value;
     var experience2 = document.getElementById('experience2').value;
     var skills = document.getElementById('skills').value;
-    // Create an object to store the form data
+
     var userDetails = {
         firstName: firstName,
         lastName: lastName,
@@ -21,8 +21,8 @@ form.addEventListener('submit', function (event) {
         experience: [experience1, experience2],
         skills: skills,
     };
-    // Save the data in localStorage to retrieve on the other page
+
     localStorage.setItem('userDetails', JSON.stringify(userDetails));
-    // Redirect the user to the display page
+
     window.location.href = "main.html";
 });
