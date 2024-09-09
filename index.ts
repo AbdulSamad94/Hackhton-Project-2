3c67b162af9cbf93877e86c3f59159f6162e905b
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-09-09 12:49:24 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ const form = document.getElementById('detailsForm') as HTMLFormElement;
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   
-  // Collect form values
   const firstName = (document.getElementById('firstName') as HTMLInputElement).value;
   const lastName = (document.getElementById('lastName') as HTMLInputElement).value;
   const email = (document.getElementById('email') as HTMLInputElement).value;
@@ -14,7 +13,6 @@ form.addEventListener('submit', (event) => {
   const experience2 = (document.getElementById('experience2') as HTMLInputElement).value;
   const skills = (document.getElementById('skills') as HTMLTextAreaElement).value;
 
-  // Create an object to store the form data
   const userDetails = {
     firstName,
     lastName,
@@ -25,9 +23,7 @@ form.addEventListener('submit', (event) => {
     skills,
   };
 
-  // Save the data in localStorage to retrieve on the other page
   localStorage.setItem('userDetails', JSON.stringify(userDetails));
 
-  // Redirect the user to the display page
   window.location.href = "main.html";
 });
